ea285dca7429c1309917bdcc9c482b66a074ae7a
Yurong Zhao <yurong.zhao@lianatech.com>
2020-07-31 13:33:20 +0300

    build code

---
+++ build/precache-manifest.4335508ee45460d201c9bdf1f34d4ade.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1c559fd532109330eda3a12e8d503d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "af224fe204dfa289aefd",
+    "url": "/static/css/main.6aeff19a.chunk.css"
+  },
+  {
+    "revision": "9d4130594c99e01be11a",
+    "url": "/static/js/2.1b960249.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b960249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af224fe204dfa289aefd",
+    "url": "/static/js/main.247beb5e.chunk.js"
+  },
+  {
+    "revision": "6be06fbc24171e2fd061",
+    "url": "/static/js/runtime-main.110b1f99.js"
+  }
+]);
---
+++ build/precache-manifest.70b9865ebfe2e6001ee7b9b93f15a90c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec8934f911cfc653d5f3c15cbfd6038",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a8b04c782710d6482370",
+    "url": "/static/css/main.6aeff19a.chunk.css"
+  },
+  {
+    "revision": "9d4130594c99e01be11a",
+    "url": "/static/js/2.1b960249.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1b960249.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8b04c782710d6482370",
+    "url": "/static/js/main.8f2bf9f9.chunk.js"
+  },
+  {
+    "revision": "6be06fbc24171e2fd061",
+    "url": "/static/js/runtime-main.110b1f99.js"
+  }
+]);
